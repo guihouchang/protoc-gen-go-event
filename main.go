a62f9c44990b47c168c464a28577cec4671e98f0
guihouchang <guihouchang@163.com>
2022-07-01 15:28:46 +0800

    # feat: 新增订阅生成器匿名函数，用于自由提供subscriber

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	release     = "0.0.5"
+	release     = "0.0.6"
 	showVersion = flag.Bool("version", false, "print the version and exit")
 	omitempty   = flag.Bool("omitempty", true, "omit if google.api is empty")
 )
